885e972931bd0ac6212957d7df2337eac49a9891
Mae_East <maeeast@gmail.com>
2016-02-28 18:12:21 -0500

    second week 203 push

---
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying case studies archives
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate_Theme
+ * @since Accelerate Theme 1.1
+ */
+
+get_header(); ?>
+
+ 	<div id="primary" class="site-content">
+ 		<div id="content" role="main">
+        <?php while ( have_posts() ) : the_post();
+          $services = get_field('services');
+          $client = get_field('client');
+          $link = get_field('site_link');
+          $image_1 = get_field('image_1');
+          $size = 'full';?>
+
+          <article class="case-study">
+            <aside class="case-study-sidebar">
+              <h2><?php the_title(); ?></h2>
+              <h5><?php echo $services; ?></h5>
+              <?php the_excerpt(); ?>
+              <p><strong><a href="<?php the_permalink(); ?>">View Project</a></strong></p>
+            </aside>
+            <div class="case-study-images">
+              <div class="case-study-images">
+                <?php if($image_1) {
+                  echo wp_get_attachment_image( $image_1, $size );
+                } ?>
+              </div>
+            </div>
+          </article>
+ 			<?php endwhile; // end of the loop. ?>
+ 		</div><!-- #content -->
+ 	</div><!-- #primary -->
+ <?php get_footer(); ?>
