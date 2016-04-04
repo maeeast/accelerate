c1637554da7259ca9715ec3952ab8601958b1b0b
Mae_East <maeeast@gmail.com>
2016-04-03 23:15:09 -0400

    add social sharing buttons

--- wp-content/themes/accelerate-theme/sidebar.php
+++ wp-content/themes/accelerate-theme/sidebar.php
@@ -9,14 +9,14 @@
 ?>
 			<aside class="sidebar">
 	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-		<?php dynamic_sidebar( 'sidebar-1' ); ?>	
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
 			<?php endif; ?>
 
 		<div class="post-author">
-					<div class="avatar"><img src="upload/Emily2.jpg" alt=""></div>
-					<div class="intro-text">I’m Emily, the lead developer at <a href="http://skillcrush.com/">Skillcrush</a> as well as an instructor in Ruby and JavaScript. This is my blog full of inspiration.</div>
+					<div class="avatar"><img src="<?php echo home_url(); ?>/wp-content/uploads/2016/04/avatar.jpg"></div>
+					<div class="intro-text">I’m Emily, the Marketing Manager at <a href="<?php echo home_url(); ?>">Accelereate</a> as well as a client liason. This is my blog full of inspiration.</div>
 				</div>
-			
+
 				<div class="widget widget_search-form">
 					<form role="search" method="get" class="search-form" action="">
 						<input type="text" class="input" placeholder="search" value="" name="s">
@@ -25,7 +25,7 @@
 				</div>
 
 				<div class="widget widget_categories">
-					<h3 class="widget-title">Categories</h3>		
+					<h3 class="widget-title">Categories</h3>
 					<ul>
 						<li><a href="">Books </a></li>
 						<li><a href="">My Notes</a></li>
@@ -36,7 +36,7 @@
 				</div>
 
 				<div class="widget widget_archives">
-					<h3 class="widget-title">Archives</h3>		
+					<h3 class="widget-title">Archives</h3>
 					<ul>
 						<li><a href="">September 2014</a></li>
 						<li><a href="">August 2014</a></li>
@@ -46,13 +46,6 @@
 					</ul>
 				</div>
 
-				<div class="widget widget_events">
-					<h3 class="widget-title">Events</h3>		
-					<ul>
-						<li><a href=""><img src="upload/Event-1.jpg" alt=""></a></li>
-						<li><a href=""><img src="upload/Event-2.jpg" alt=""></a></li>
-						<li><a href=""><img src="upload/Event-3.jpg" alt=""></a></li>
-					</ul>
-				</div>
-	
+
+
 </aside>
